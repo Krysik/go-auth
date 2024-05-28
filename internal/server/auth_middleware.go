e04d9cfad9ea18130208317fe99c5056bb140ae2
Marcin Krysinski <marcin_krysinski@outlook.com>
2024-05-28 19:38:45 +0200

    feat: auth middleware

---
+++ internal/server/auth_middleware.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/Krysik/go-auth/internal/server/auth"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+)
+
+type AuthContext struct {
+	echo.Context
+	AccountId string
+}
+
+func newAuthMiddlewareContext(next echo.HandlerFunc, issuer string) echo.HandlerFunc {
+	// fmt.Println(ac.GE)
+	return func(ctx echo.Context) error {
+		accessTokenCookie, err := ctx.Request().Cookie("accessToken")
+
+		if err != nil {
+			ctx.Logger().Warn("failed to get access token cookie ", err.Error())
+			return ctx.JSON(http.StatusUnauthorized, HttpErrorResponse{
+				Errors: []HttpError{
+					{
+						Code:    "UNAUTHORIZED",
+						Title:   "Unauthorized",
+						Details: "Invalid credentials",
+					},
+				},
+			})
+		}
+
+		accessToken := accessTokenCookie.Value
+
+		token, err := jwt.ParseWithClaims(accessToken, &auth.TokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+			// token.Claims.
+			return []byte("top-secret"), nil
+		}, jwt.WithIssuer(issuer), jwt.WithExpirationRequired())
+
+		if err != nil {
+			ctx.Logger().Warn("failed to parse access token ", err.Error())
+			return ctx.JSON(http.StatusUnauthorized, HttpErrorResponse{
+				Errors: []HttpError{
+					{
+						Code:    "UNAUTHORIZED",
+						Title:   "Unauthorized",
+						Details: "Invalid credentials",
+					},
+				},
+			})
+		}
+
+		claims := token.Claims.(*auth.TokenClaims)
+		accountId := claims.Subject
+
+		ac := &AuthContext{
+			AccountId: accountId,
+			Context:   ctx,
+		}
+
+		return next(ac)
+	}
+}
